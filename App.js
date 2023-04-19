234a68fc2ab3c2f4a140490a42d9d2cf3741d4cd
Djibril-6et <djibril.6et@gmail.com>
2023-04-19 23:25:18 +0200

    splash screen init

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/config/styledTheme";
 import "react-native-gesture-handler";
@@ -11,12 +11,19 @@ import { persistor, store } from "./src/redux/config/store";
 import { LanguageProvider } from "./src/traduction/LanguageContext";
 import I18n from "./src/traduction/i18n";
 import Header from "./src/components/Header"
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
   const [language, setLanguage] = useState("en");
 
   const [myTheme, setTheme] = useState("dark");
   const isDarkTheme = myTheme === 'dark'
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+  }, []);
 
   const toggleTheme = () => {
     setTheme(isDarkTheme ? 'light' : 'dark')
--- ios/myScanReader/AppDelegate.mm
+++ ios/myScanReader/AppDelegate.mm
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import "RNSplashScreen.h"
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -10,8 +10,10 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [RNSplashScreen show];
+  return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
@@ -30,6 +32,9 @@
 /// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
 - (BOOL)concurrentRootEnabled
 {
+  // [RNSplashScreen show]; // here
+  // or
+  //[RNSplashScreen showSplash:@"LaunchScreen" inRootView:rootView];
   return true;
 }
 
