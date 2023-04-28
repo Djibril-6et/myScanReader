2c136d988509c43b0d1ca60fe5d615436c60c0e1
Djibril-6et <djibril.6et@gmail.com>
2023-04-28 11:20:28 +0200

    change theme again

--- src/config/styledTheme.js
+++ src/config/styledTheme.js
@@ -1,15 +1,14 @@
 const darkTheme = {
     globalPrimaryColor: '#696969',
-    // primaryColor: '#0f0c29',
-    primaryColor: '#0A0A0A',
-    secondaryColor: '#EB4034',
+    primaryColor: '#0f0c29',
+    secondaryColor: '#A9A9A9',
     text: '#fff',
 }
 
 const lightTheme = {
     globalPrimaryColor: '#696969',
     primaryColor: '#f8f8f8',
-    secondaryColor: '#EB4034',
+    secondaryColor: '#A9A9A9',
     text: '#000',
 }
 
